8cd963735a4af8b37b3ed22cd18e375af7ae4110
Namsoo315 <gustn2130@naver.com>
2025-12-29 17:39:48 +0900

    fix: h2 테스트 제거 및 연동및 CI 통과 되게끔 구성

--- closet/src/test/java/com/codeit/closet/ClosetApplicationTests.java
+++ closet/src/test/java/com/codeit/closet/ClosetApplicationTests.java
@@ -2,8 +2,10 @@ package com.codeit.closet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ClosetApplicationTests {
 
   @Test
