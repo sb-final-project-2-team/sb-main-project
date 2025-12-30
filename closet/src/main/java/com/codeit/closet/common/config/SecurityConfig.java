7871e68512ad26aa2bc0747b044a0f3871a7d8bb
Namsoo315 <gustn2130@naver.com>
2025-12-30 13:30:21 +0900

    feat: CORS 허용 처리

--- closet/src/main/java/com/codeit/closet/common/config/SecurityConfig.java
+++ closet/src/main/java/com/codeit/closet/common/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.codeit.closet.common.config;
 
 import com.codeit.closet.common.security.SpaCsrfTokenRequestHandler;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -11,6 +12,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -20,12 +24,18 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
+        // CSRF 사용용 설정
         .csrf(csrf -> csrf
             .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
             .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler()))
+
+        // 권한 범위 허용
         .authorizeHttpRequests(auth -> auth
             .anyRequest().permitAll()
         )
+
+        // Cors 설정
+        .cors(Customizer.withDefaults())
         .httpBasic(Customizer.withDefaults());
 
     return http.build();
@@ -36,5 +46,26 @@ public class SecurityConfig {
     return new BCryptPasswordEncoder();
   }
 
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration config = new CorsConfiguration();
+
+    config.setAllowedOrigins(List.of("http://localhost:3000"));
+
+    config.setAllowedMethods(List.of(
+        "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"
+    ));
+
+    config.setAllowedHeaders(List.of("*"));
+    config.setExposedHeaders(List.of("Authorization"));
+
+    config.setAllowCredentials(true); // 🔥 쿠키 사용 시 필수
+
+    UrlBasedCorsConfigurationSource source =
+        new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+
+    return source;
+  }
 
 }
