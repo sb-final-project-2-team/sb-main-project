45a0b52ec5b1470a6258c624fa40498e89a84051
backKim1024 <dydgml7313@naver.com>
2025-12-30 17:51:04 +0900

    feat: 알림(notification) 엔티티 생성

---
+++ closet/src/main/java/com/codeit/closet/module/notification/entity/notification.java
@@ -0,0 +1,31 @@
+package com.codeit.closet.module.notification.entity;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "notifications")
+public class notification {
+	@Id
+	@GeneratedValue
+	@Column(columnDefinition = "uuid")
+	private UUID id;
+
+	@Column(nullable = false, updatable = false)
+	private UUID receiverId;
+
+	@Column(nullable = false, length = 200)
+	private String title;
+
+	@Column(nullable = false, columnDefinition = "TEXT")
+	private String content;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private NotificationLevel level;
+
+	@Column(nullable = false, updatable = false)
+	private Instant created_at;
+}
