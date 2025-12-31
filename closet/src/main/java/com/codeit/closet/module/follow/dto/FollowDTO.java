6c3caade5016f5a4c3473ad9438f54dd217076da
5ranke <wjdtnwls0106@gmail.com>
2025-12-31 13:40:24 +0900

    feat: UserSummary 타입 연결

--- closet/src/main/java/com/codeit/closet/module/follow/dto/FollowDTO.java
+++ closet/src/main/java/com/codeit/closet/module/follow/dto/FollowDTO.java
@@ -1,9 +1,11 @@
 package com.codeit.closet.module.follow.dto;
 
+import com.codeit.closet.module.user.dto.user.UserSummary;
+
 import java.util.UUID;
 
 public record FollowDTO(
-        UUID id
-//        UserSummary followee,
-//        UserSummary follower
+        UUID id,
+        UserSummary followee,
+        UserSummary follower
 ) {}
