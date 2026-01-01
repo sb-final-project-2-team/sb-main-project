fc4cd4baae55d144568cc785c9af89778217659f
backKim1024 <dydgml7313@naver.com>
2026-01-02 07:19:59 +0900

    refactor: Notification 엔티티 수정

--- closet/src/main/java/com/codeit/closet/module/notification/entity/Notification.java
+++ closet/src/main/java/com/codeit/closet/module/notification/entity/Notification.java
@@ -4,10 +4,20 @@ import java.time.Instant;
 import java.util.UUID;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "notifications")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class Notification {
+
 	@Id
 	@GeneratedValue
 	@Column(columnDefinition = "uuid")
@@ -27,5 +37,10 @@ public class Notification {
 	private NotificationLevel level;
 
 	@Column(nullable = false, updatable = false)
-	private Instant created_at;
+	private Instant createdAt;
+
+	@PrePersist
+	protected void onCreate() {
+		this.createdAt = Instant.now(); // 생성시점에 자동으로 시간 기록
+	}
 }
