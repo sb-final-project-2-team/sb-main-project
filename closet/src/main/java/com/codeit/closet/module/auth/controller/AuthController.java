d74269bb346da1df608d798d171921ccf4f308fb
Namsoo315 <gustn2130@naver.com>
2026-01-01 21:27:20 +0900

    feat: Refresh API 생성

--- closet/src/main/java/com/codeit/closet/module/auth/controller/AuthController.java
+++ closet/src/main/java/com/codeit/closet/module/auth/controller/AuthController.java
@@ -1,11 +1,17 @@
 package com.codeit.closet.module.auth.controller;
 
+import com.codeit.closet.common.security.jwt.JwtDTO;
+import com.codeit.closet.common.security.jwt.JwtInformation;
+import com.codeit.closet.common.security.jwt.JwtTokenProvider;
 import com.codeit.closet.module.auth.service.AuthService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +22,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
-  // private final AuthService authService;
+
+  private final AuthService authService;
+  private final JwtTokenProvider jwtTokenProvider;
 
   @GetMapping("/csrf-token")
   public ResponseEntity<Void> getCsrfToken(CsrfToken csrfToken) {
@@ -25,9 +33,20 @@ public class AuthController {
 
     return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
   }
-  // 토큰 재발급
-  //@PostMapping("/refresh")
 
+  @PostMapping("/refresh")
+  public ResponseEntity<JwtDTO> createRefreshToken(
+      @CookieValue(JwtTokenProvider.REFRESH_TOKEN_COOKIE_NAME) String refreshToken,
+      HttpServletResponse response) {
+    log.info("Refresh Token: {}", refreshToken);
+    JwtInformation jwtInformation = authService.refreshToken(refreshToken);
+    Cookie cookie = jwtTokenProvider.generateRefreshTokenCookie(jwtInformation.getRefreshToken());
+    response.addCookie(cookie);
+
+    JwtDTO jwtDTO = new JwtDTO(jwtInformation.getUserDTO(), jwtInformation.getAccessToken());
+
+    return ResponseEntity.status(HttpStatus.OK).body(jwtDTO);
+  }
   // 비밀번호 초기화
   //@PostMapping("/reset-password")
 }
--- closet/src/main/java/com/codeit/closet/module/auth/service/impl/BasicAuthService.java
+++ closet/src/main/java/com/codeit/closet/module/auth/service/impl/BasicAuthService.java
@@ -1,11 +1,58 @@
 package com.codeit.closet.module.auth.service.impl;
 
+import com.codeit.closet.common.security.ClosetUserDetails;
+import com.codeit.closet.common.security.jwt.JwtInformation;
+import com.codeit.closet.common.security.jwt.JwtRegistry;
+import com.codeit.closet.common.security.jwt.JwtTokenProvider;
 import com.codeit.closet.module.auth.service.AuthService;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class BasicAuthService implements AuthService {
 
+  private final JwtTokenProvider jwtTokenProvider;
+  private final JwtRegistry<UUID> jwtRegistry;
+  private final UserDetailsService userDetailsService;
+
+  @Override
+  public JwtInformation refreshToken(String refreshToken) {
+    if (!jwtTokenProvider.validateRefreshToken(refreshToken)
+    || !jwtRegistry.hasActiveJwtInformationByRefreshToken(refreshToken)) {
+      throw new RuntimeException("refreshToken이 만료되었거나 존재하지 않습니다.");
+    }
+
+    String email = jwtTokenProvider.getEmailFromToken(refreshToken);
+    UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+
+    if (userDetails == null) {
+      throw new UsernameNotFoundException("User not found");
+    }
+
+    try {
+      ClosetUserDetails closetUserDetails = (ClosetUserDetails) userDetails;
+      String newAccessToken = jwtTokenProvider.generateAccessToken(closetUserDetails);
+      String newRefreshToken = jwtTokenProvider.generateRefreshToken(closetUserDetails);
+
+      log.info("new RefreshToken : {}", newRefreshToken);
+      JwtInformation newJwtInformation = new JwtInformation(
+          closetUserDetails.getUserDTO(),
+          newAccessToken,
+          newRefreshToken
+      );
+
+      jwtRegistry.rotateJwtInformation(refreshToken, newJwtInformation);
+      return newJwtInformation;
+    }catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
+  }
 }
