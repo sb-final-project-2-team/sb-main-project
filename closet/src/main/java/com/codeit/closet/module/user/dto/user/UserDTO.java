13dd36eb5b541709eca7105374656b85aab14c85
Namsoo315 <gustn2130@naver.com>
2026-01-04 23:27:59 +0900

    feat: 코드리뷰 반영

--- closet/src/main/java/com/codeit/closet/module/user/dto/user/UserDTO.java
+++ closet/src/main/java/com/codeit/closet/module/user/dto/user/UserDTO.java
@@ -1,6 +1,5 @@
 package com.codeit.closet.module.user.dto.user;
 
-import com.codeit.closet.module.user.entity.User;
 import com.codeit.closet.module.user.entity.UserRole;
 
 import java.time.Instant;
@@ -14,14 +13,4 @@ public record UserDTO(
         UserRole role,
         Boolean locked
 ) {
-  public static UserDTO from(User user) {
-    return new UserDTO(
-        user.getId(),
-        user.getCreatedAt(),
-        user.getEmail(),
-        user.getName(),
-        user.getRole(),
-        user.getLocked()
-    );
-  }
 }
