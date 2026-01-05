024992e295125acd86d532c93d46ee46bb151394
5ranke <wjdtnwls0106@gmail.com>
2026-01-05 17:49:21 +0900

    feat: int -> Integer 변경

--- closet/src/main/java/com/codeit/closet/module/follow/service/FollowService.java
+++ closet/src/main/java/com/codeit/closet/module/follow/service/FollowService.java
@@ -16,7 +16,7 @@ public interface FollowService {
             UUID followerId,
             String cursor,
             UUID idAfter,
-            int limit,
+            Integer limit,
             String nameLike
     );
 
@@ -24,7 +24,7 @@ public interface FollowService {
             UUID followeeId,
             String cursor,
             UUID idAfter,
-            int limit,
+            Integer limit,
             String nameLike
     );
 
