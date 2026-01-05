4abc2d825b1a736ed4b24f2321bb19c5d284f911
Namsoo315 <gustn2130@naver.com>
2026-01-05 15:20:55 +0900

    fix: coderabbit 리뷰 반영

--- closet/src/main/java/com/codeit/closet/module/like/entity/Like.java
+++ closet/src/main/java/com/codeit/closet/module/like/entity/Like.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import java.time.Instant;
 import java.util.UUID;
 import lombok.AccessLevel;
@@ -16,7 +17,9 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
-@Table(name = "likes")
+@Table(name = "likes", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"user_id", "feed_id"})
+})
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
