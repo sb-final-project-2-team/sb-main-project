bb307162039c94cb5f968b6fbc5a2f8914a4be35
backKim1024 <dydgml7313@naver.com>
2026-01-07 20:05:14 +0900

    feat: 다건, 단건 처리를 위한 NotificationService 인터페이스 정의
    - 단일 대상 알림 생성
    : 팔로우, 좋아요, 댓글, 권한변경, DM)
    - 다수 대상 알림 생성
    : 날씨 변화 알림

--- closet/src/main/java/com/codeit/closet/module/notification/service/NotificationService.java
+++ closet/src/main/java/com/codeit/closet/module/notification/service/NotificationService.java
@@ -1,6 +1,7 @@
 package com.codeit.closet.module.notification.service;
 
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 
@@ -12,5 +13,11 @@ public interface NotificationService {
 	List<NotificationDTO> findAllByReceiverId(UUID receiverId);
 
 	void deleteNotification(UUID id, UUID receiverId);
+
+	// (팔로우, 좋아요, 댓글, 권한변경, DM)
+	void create(UUID receiverId, String title, String content);
+
+	// (급격한 날씨 변화 알림)
+	void createMany(Set<UUID> receiverIds, String title, String content);
 
 }
