340c34b923abc5252942b84c78ba248e883090ed
backKim1024 <dydgml7313@naver.com>
2026-01-07 19:51:51 +0900

    feat: Kafka 기반의 알림 이벤트 발행 구현체 추가
    body
    - NotificationEventPublisher 인터페이스의 Kafka 구현체 작성
    - 수신자 ID(receiverId)를 메시지 키로 사용하여 파티션 순서 보장
    - 이벤트 발행 시 로깅(info)

---
+++ closet/src/main/java/com/codeit/closet/module/notification/event/KafkaNotificationEventPublisher.java
@@ -0,0 +1,32 @@
+package com.codeit.closet.module.notification.event;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class KafkaNotificationEventPublisher implements NotificationEventPublisher {
+	private final KafkaTemplate<String, NotificationEvent> kafkaTemplate;
+
+	@Override
+	public void publish(NotificationEvent event) {
+
+		String notificationKey = event.receiverId().toString(); // 수신자 기준으로 지정
+
+		kafkaTemplate.send(
+			NotificationTopics.NOTIFICATION_CREATED,
+			notificationKey,
+			event
+		);
+
+		log.info(
+			"[Kafka] 알림 생성 이벤트 발행 완료 (receiverId={}, notificationId={})",
+			notificationKey,
+			event.id()
+		);
+	}
+}
