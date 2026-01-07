952582fff8c1d05469e52e06e9af9f6ab6d22ebf
Namsoo315 <gustn2130@naver.com>
2026-01-07 13:36:59 +0900

    feat: 비밀번호 초기화 작업 추가.

--- closet/src/main/java/com/codeit/closet/module/auth/service/impl/BasicAuthService.java
+++ closet/src/main/java/com/codeit/closet/module/auth/service/impl/BasicAuthService.java
@@ -1,22 +1,34 @@
 package com.codeit.closet.module.auth.service.impl;
 
+import com.codeit.closet.common.mail.service.MailService;
 import com.codeit.closet.common.security.ClosetUserDetails;
 import com.codeit.closet.common.security.jwt.JwtInformation;
 import com.codeit.closet.common.security.jwt.JwtRegistry;
 import com.codeit.closet.common.security.jwt.JwtTokenProvider;
+import com.codeit.closet.module.auth.dto.ResetPasswordRequest;
 import com.codeit.closet.module.auth.service.AuthService;
+import com.codeit.closet.module.user.entity.User;
+import com.codeit.closet.module.user.repository.UserRepository;
+import java.util.NoSuchElementException;
+import java.util.Random;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class BasicAuthService implements AuthService {
+
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+  private final MailService mailService;
 
   private final JwtTokenProvider jwtTokenProvider;
   private final JwtRegistry<UUID> jwtRegistry;
@@ -54,5 +66,35 @@ public class BasicAuthService implements AuthService {
       throw new RuntimeException(e);
     }
 
+  }
+
+  @Override
+  @Transactional
+  public void resetPassword(ResetPasswordRequest request) {
+    User user = userRepository.findByEmail(request.email()).orElseThrow(
+        () -> new NoSuchElementException("존재하지 않는 회원입니다."));
+
+    String tempPassword = generateRandomString(8);
+
+    user.updateTempPassword(passwordEncoder.encode(tempPassword));
+
+    mailService.sendResetPasswordMail(user.getEmail(), tempPassword);
+  }
+
+  public static String generateRandomString(int length) {
+    String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    Random random = new Random();
+
+    StringBuilder sb = new StringBuilder(length);
+
+    for (int i = 0; i < length; i++) {
+      int randomIndex = random.nextInt(characters.length());
+      char randomChar = characters.charAt(randomIndex);
+
+      sb.append(randomChar);
+    }
+
+    return sb.toString();
   }
 }
