a1e58d7bdd6a3d58e161a9388839babc66b4a9e9
5ranke <wjdtnwls0106@gmail.com>
2026-01-12 11:53:38 +0900

    fix: 저장api 호출 관련 csrf 설정 변경

--- closet/src/main/java/com/codeit/closet/common/config/SecurityConfig.java
+++ closet/src/main/java/com/codeit/closet/common/config/SecurityConfig.java
@@ -69,13 +69,9 @@ public class SecurityConfig {
             .successHandler(oAuth2SuccessHandler)
         )
 
-        .logout(logout -> logout
-            .logoutUrl("/api/auth/sign-out")
-            .addLogoutHandler(jwtLogoutHandler)
-            .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler()))
-
         // CSRF 사용용 설정
         .csrf(csrf -> csrf
+            .ignoringRequestMatchers("/api/direct-messages/**") // dm 저장 요청
             .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
             .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler()))
 
